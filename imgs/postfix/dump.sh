130c1671d7447b3f068e9c9b3a9e849498df6235
Ronmi Ren <ronmi.ren@gmail.com>
2017-06-06 09:30:54 +0800

    fix misplacement

--- imgs/postfix/dump.sh
+++ imgs/postfix/dump.sh
@@ -8,13 +8,6 @@
 # Non-Stop:       yes
 ### END INIT INFO
 
-# this is for fail2ban
-if [[ ! -f /var/log/mail.warn ]]
-then
-    touch /var/log/mail.warn
-    chown root:adm /var/log/mail.warn
-fi
-
 rm -f /run/dump
 mkfifo /run/dump
 cat /run/dump
